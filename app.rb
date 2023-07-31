59f642998b6481c2d54da306812136ebdb98fa23
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-07-31 19:59:01 +0000

    Start: add route /rock and embedded pages for /rock. Doesn't work as expected

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:layout)
+end
+
+
+get("/rock") do
+  "<h1>hello</h1>"
+  i = rand(0..2)
+  
+  rps_opp_rock = ["rock", "paper", "scizzors"]
+
+  if rps_opp_rock[i] == "rock"
+    @outcome_rock = "We tied"
+  elsif rps_opp_rock[i] == "paper"
+    @outcome_rock = "We lost"
+  else
+    @outcome_rock = "We won"
+  end
+  erb(:rock) #doesn't work and colors aren't on
 end
